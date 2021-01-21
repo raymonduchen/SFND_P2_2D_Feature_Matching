b9ef63bfe7edb5cc70627ace52549c74243fa3e1
Raymond Chen <raymonduchen@gmail.com>
2021-01-21 12:35:09 +0800

    Add FLANN matcher

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -183,9 +183,10 @@ int main(int argc, const char *argv[])
             /* MATCH KEYPOINT DESCRIPTORS */
 
             vector<cv::DMatch> matches;
-            string matcherType = "MAT_BF";        // MAT_BF, MAT_FLANN
             //string descriptorType = "DES_BINARY"; // DES_BINARY, DES_HOG
             string descriptorType = "DES_HOG";
+            //string matcherType = "MAT_BF";        // MAT_BF, MAT_FLANN
+            string matcherType = "MAT_FLANN";        // MAT_BF, MAT_FLANN
             string selectorType = "SEL_NN";       // SEL_NN, SEL_KNN
 
             //// STUDENT ASSIGNMENT
